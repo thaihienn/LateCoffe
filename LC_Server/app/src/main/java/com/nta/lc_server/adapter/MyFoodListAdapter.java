74e05bc6a44a713c3dc5a27aa37ceb8b91140bc2
Thai Hien <hiendt0503@gmail.com>
2020-11-05 14:13:03 +0700

    test

---
+++ LC_Server/app/src/main/java/com/nta/lc_server/adapter/MyFoodListAdapter.java
@@ -0,0 +1,98 @@
+package com.nta.lc_server.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.nta.lc_server.R;
+import com.nta.lc_server.callback.IRecyclerClickListener;
+import com.nta.lc_server.common.Common;
+import com.nta.lc_server.model.FoodModel;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public class MyFoodListAdapter extends RecyclerView.Adapter<MyFoodListAdapter.MyViewHolder> {
+
+    private Context context;
+    private List<FoodModel> foodModelList;
+
+    public MyFoodListAdapter(Context context, List<FoodModel> foodModelList) {
+        this.context = context;
+        this.foodModelList = foodModelList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new MyViewHolder(LayoutInflater.from(context)
+                .inflate(R.layout.layout_food_item, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Glide.with(context).load(foodModelList.get(position).getImage()).into(holder.img_food_image);
+        holder.txt_food_price.setText(new StringBuilder("$")
+                .append(foodModelList.get(position).getPrice()));
+        holder.txt_food_name.setText(new StringBuilder("")
+                .append(foodModelList.get(position).getName()));
+
+        //Event
+        holder.setListener((view, pos) -> {
+            Common.selectedFood = foodModelList.get(pos);
+            //To assign this data for 'key' of food, we will assign when we retrieve foods from Category
+            Common.selectedFood.setKey(String.valueOf(pos));
+
+        });
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return foodModelList.size();
+    }
+
+    public FoodModel getItemAtPosition(int pos){
+        return foodModelList.get(pos);
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private Unbinder unbinder;
+        @BindView(R.id.txt_food_name)
+        TextView txt_food_name;
+        @BindView(R.id.txt_food_price)
+        TextView txt_food_price;
+        @BindView(R.id.img_food_image)
+        ImageView img_food_image;
+
+
+        IRecyclerClickListener listener;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            unbinder = ButterKnife.bind(this, itemView);
+            itemView.setOnClickListener(this);
+        }
+
+        public void setListener(IRecyclerClickListener listener) {
+            this.listener = listener;
+        }
+
+        @Override
+        public void onClick(View view) {
+            listener.onItemClickListener(view, getAdapterPosition());
+        }
+    }
+}
---
+++ LC_Shipper/app/src/main/java/com/nta/lc_shipper/common/Common.java
@@ -0,0 +1,121 @@
+package com.nta.lc_shipper.common;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.nta.lc_shipper.R;
+import com.nta.lc_shipper.model.ShipperUserModel;
+import com.nta.lc_shipper.model.TokenModel;
+
+public class Common {
+    public static final String SHIPPER_REF = "Shippers";
+    public static final String CATEGORY_REF = "Category";
+    public static final int DEFAULT_COLUMN_COUNT = 0;
+    public static final int FULL_WIDTH_COLUMN = 1;
+    public static final String ORDER_REF = "Orders";
+    public static final String NOTI_TITLE = "title";
+    public static final String NOTI_CONTENT = "content";
+    public static final String TOKEN_REF = "Tokens";
+    public static final String SHIPPER_ORDER_REF = "ShippingOrder"; //Same as server app
+
+    public static ShipperUserModel currentShipperUser;
+
+    //Set Span String
+    public static void setSpanString(String welcome, String name, TextView textView) {
+        SpannableStringBuilder builder = new SpannableStringBuilder();
+        builder.append(welcome);
+        SpannableString spannableString = new SpannableString(name);
+        StyleSpan boldSpan = new StyleSpan(Typeface.BOLD);
+        spannableString.setSpan(boldSpan, 0, name.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        builder.append(spannableString);
+        textView.setText(builder, TextView.BufferType.SPANNABLE);
+    }
+
+    public static void setSpanStringColor(String welcome, String name, TextView textView, int color) {
+        SpannableStringBuilder builder = new SpannableStringBuilder();
+        builder.append(welcome);
+        SpannableString spannableString = new SpannableString(name);
+        StyleSpan boldSpan = new StyleSpan(Typeface.BOLD);
+        spannableString.setSpan(boldSpan, 0, name.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(color), 0, name.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        builder.append(spannableString);
+        textView.setText(builder, TextView.BufferType.SPANNABLE);
+    }
+
+    public static String convertStatusToString(int orderStatus) {
+        switch (orderStatus)
+        {
+            case 0:
+                return "Placed";
+            case 1:
+                return "Shipping";
+            case 2:
+                return "Shipped";
+            case -1:
+                return "Cancelled";
+            default:
+                return "Error";
+        }
+    }
+
+    public static void showNotification(Context context, int id, String title, String content, Intent intent) {
+        PendingIntent pendingIntent = null;
+        if (intent != null)
+            pendingIntent = PendingIntent.getActivity(context, id, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        String NOTIFICATION_CHANNEL_ID = "late_coffee";
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Late Coffee", NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setDescription("Late Coffee");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(new long[]{0, 1000, 500, 1000});
+            notificationChannel.enableVibration(true);
+
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID);
+        builder.setContentTitle(title)
+                .setContentText(content)
+                .setAutoCancel(true)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_restaurant_menu));
+        if (pendingIntent != null)
+            builder.setContentIntent(pendingIntent);
+        Notification notification = builder.build();
+        notificationManager.notify(id, notification);
+
+    }
+
+    public static void updateToken(Context context, String newToken) {
+        FirebaseDatabase.getInstance()
+                .getReference(Common.TOKEN_REF)
+                .child(Common.currentShipperUser.getUid())
+                .setValue(new TokenModel(Common.currentShipperUser.getPhone(), newToken))
+                .addOnFailureListener(e -> {
+                    Toast.makeText(context, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                });
+    }
+
+    public static String createTopicOrder() {
+        return new StringBuilder("/topics/new_order").toString();
+    }
+}
